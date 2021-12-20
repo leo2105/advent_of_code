fe67ee11b9d0fde931b491e941cb7e23f5fce1fc
leo2105 <leoleonvera20@hotmail.com>
2021-12-20 18:18:16 -0500

    day9 part1 done

--- day9/program9.py
+++ day9/program9.py
@@ -1,9 +1,11 @@
 import numpy as np
-f = open('1.txt', 'r')
+f = open('2.txt', 'r')
 
 lines = f.read().split('\n')
 
 dx, dy = [0,1,0,-1], [-1,0,1,0]
+arr_i, arr_j = [], []
+suma = 0
 
 def valid_coord(x, y):
     if x == len(lines[0]) or x < 0:
@@ -14,7 +16,17 @@ def valid_coord(x, y):
 
 for i in range(len(lines)):
     for j in range(len(lines[0])):
+        flag = True
         for k in range(4):
-            x_, y_ = lines[i][j] + dx[k], lines[i][j]
-            if valid_coord(x, y):
-                
+            j_, i_ = j + dx[k], i + dy[k]
+            if not valid_coord(j_, i_):
+                continue
+            if lines[i][j] >= lines[i_][j_]:
+                flag = False
+        if flag:
+            #arr_i.append(i)
+            #arr_j.append(j)
+            suma += (int(lines[i][j])+1)
+
+print(suma)
+#print(arr_i, arr_j)
